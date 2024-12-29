e7ac433ec9be502282ca9ad707446523e4828d52
Andrei Matys <matys.andrey@gmail.com>
2024-12-29 16:39:29 +0200

    Solved problem 1757

---
+++ 1757_recandlowfat/schema.sql
@@ -0,0 +1,12 @@
+CREATE TYPE product_bool as ENUM ('Y', 'N');
+CREATE TABLE IF NOT EXISTS Products (
+	product_id int, 
+	low_fats product_bool, 
+	recyclable product_bool
+);
+TRUNCATE TABLE Products;
+INSERT INTO Products (product_id, low_fats, recyclable) VALUES ('0', 'Y', 'N');
+INSERT INTO Products (product_id, low_fats, recyclable) VALUES ('1', 'Y', 'Y');
+INSERT INTO Products (product_id, low_fats, recyclable) VALUES ('2', 'N', 'Y');
+INSERT INTO Products (product_id, low_fats, recyclable) VALUES ('3', 'Y', 'Y');
+INSERT INTO Products (product_id, low_fats, recyclable) VALUES ('4', 'N', 'N');
